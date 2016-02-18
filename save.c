3ebcf3c886f966884fe1a0e455b8c10eab5e7fe0
lijiyong0303 <lijiyong0303@outlook.com>
2016-02-17 18:06:37 -0800

    rewrite the storage function,but no compile and run

--- save.c
+++ save.c
@@ -5,112 +5,327 @@
 #include "bptree.h"
 #include "bitmap.h"
 #include "wireway.h"
-
-#define block_size sizeof(node)
-#define node_num 1000
-#define index_file_size  node_num*block_size
-#define zone_index_mask (0xFFULL << (BITS_PER_LONG - BITS_PER_BYTE))
-
-enum zone_type
-{
-    bptree_type,
-    key_type,
-    wireway_type, 
-};
+#include "storage_sys.h"
 
 
-
-typedef struct file_header_tag
-{
-    int node_count;
-    int root_id;
-    long bitmap[(node_num/sizeof(long))+1];       
-}file_head;
-
-typedef struct storage_zone_tag
-{
-    char zone_name[32];
-    int  zone_id;
-    int  zone_type;
-    int  size;
-    int  total_count;
-    int  used_count;
-    file_head *head;
-    FILE *file;
-    struct storage_zone_tag *next;
-}storage_zone;
-
-storage_zone zone_list[] =
-{
-    {.zone_name = "storage_list.mdf",.zone_id = 0,.zone_type = bptree_type,.size = sizeof(node_block),.total_count = 1000 ,.used_count = 0,.file = NULL,.next = NULL},
-    {.zone_name = "storage_key.mdf",.zone_id = 0,.zone_type = key_type,.size = 32,.total_count = 1000 ,.used_count = 0,.file = NULL,.next = NULL},
-    
-    {.zone_name = "storage_wireway.mdf",.zone_id = 0,.zone_type = wireway_type,.size = sizeof(wireway_block),.total_count = 2000 ,.used_count = 0,.file = NULL,.next = NULL}
-};
+storage_zone_head *zone_head = NULL;
+storage_buddy_head *buddy_head = NULL;
+int global_zone_id = 0;
 
 unsigned long alloc_zone_block(storage_zone *zone,unsigned long idx);
-int disk_storage_init()
+
+void free_storage_zone(storage_zone *zone)
 {
-    FILE *f = NULL;
-    int size = 0;
-    int i = 0;
-    int fd = 0;
-    int res =1;
-    file_head *head;
-    
-    for(i = 0 ; i < sizeof(zone_list)/sizeof(storage_zone) ; i++)
+    storage_media *media,*media_tmp;
+    if(NULL == zone)
     {
+        return ;
+    }
 
-        head = malloc(sizeof(file_head));
-        if(!head)
-        {
-            return 1;
-        }
+    media = zone->media_list;
+    while(media)
+    {
+        media_tmp = media;
+        media = media->next;
+        free(media_tmp);
+    }    
 
-        if(access(zone_list[i].zone_name,0)!= 0)
-        {
-            f = fopen(zone_list[i].zone_name,"w+");
-            if(f)
-            {
-                fd = fileno(f);
-                int res =posix_fallocate(fd,0,zone_list[i].total_count *zone_list[i].size +sizeof(file_head));
-                printf("result is %d\r\n",res);
-                memset(head,0,sizeof(file_head));
-                head->root_id = -1;
-                fwrite(head,sizeof(file_head),1,f);
-                fclose(f);
+    free(zone);
+    return;
+}
+
+storage_zone *restore_storage_zone(char *zone_name)/*zone_name file  exist*/
+{
+    FILE *file;
+    int fd;
+    storage_zone *zone;
+    int i;
+    storage_media *media ,*media_tmp;
+    char tmp_file_name[32+10];
+    file = fopen(zone_name,"rb+");
+    if(file){
+        zone = malloc(sizeof(storage_zone));
+        if(zone){
+            memset(zone,0,sizeof(storage_zone));
+            fread(zone,sizeof(storage_zone),1,file);
+            zone->media_list = NULL;
+            zone->cur_media = NULL;
+            zone->next = NULL;
+            for(i = 0 ; i < zone->media_count ; i++){
+                switch(zone->zone_type)
+                {
+                    case zone_block_type:
+                    {                   
+                        int len = sizeof(storage_media)+(zone->block_count/sizeof(long) +1)*sizeof(long);
+                        media = malloc(len);
+                        if(media){
+                            if(0 == i){
+                                fseek(file,sizeof(storage_zone),SEEK_SET);
+                                fread(media,len,1,file);
+                                media->file_handle = file;
+                                media->next = NULL;
+                                media_tmp = media;
+                                zone->cur_media = media;                               
+                            } 
+                            else{
+                                sprintf(tmp_file_name,"%s#%d",zone_name,i);
+                                file = fopen(tmp_file_name,"rb+");
+                                if(file){
+                                    fread(media,len,1,file);
+                                    media->file_handle = NULL;
+                                    media->next = NULL;
+                                    media_tmp->next = media;
+                                    fclose(file);
+                                }
+                            }
+                        }                      
+                        break;
+                    }
+                    case zone_free_type:
+                    {
+                        printf("zone type un support\r\n");
+                        break;
+                    }
+                    default:
+                        printf("zone type error\r\n");
+                        break;
+                }
             }
-            else
-            {
-                return 1;
-            }
-        }
-      
-        f = NULL;
-        f = fopen(zone_list[i].zone_name,"rb+");
-        if(f)
-        {
-            fd = fileno(f);
-            zone_list[i].zone_id = fd;
-            fread(head,sizeof(file_head),1,f);
-            zone_list[i].head = head;
-            zone_list[i].file = f;            
+             
         }
         else
         {
-                return 1;
+            goto free_file;
         }
-               
-    }        
-    return 0;
-
+        return zone;
+    }
+    return NULL;
+    
+    free_file:
+    if(file) fclose(file);
+    
+    free_zone:
+    free_storage_zone(zone);
+    return NULL;
 }
-void disk_storage_exit()
+int alloc_storage_zone_id()
 {
-   // if(index_file_stream)
-   // fclose(index_file_stream);
+    return global_zone_id++; 
+}
+storage_zone *alloc_storage_zone(char *zone_name,int zone_type,int block_size,int per_media_count)
+{
+    storage_zone *zone_tmp;
+    storage_media *media,*media_tmp;
+    int len;
+    FILE *f;
+    zone_tmp = malloc(sizeof(storage_zone));
+    if(zone_tmp)
+    {
+        strcpy(zone_tmp->zone_name,zone_name);
+        zone_tmp->zone_type = zone_type;
+        zone_tmp->block_size = block_size;
+        zone_tmp->media_count = 1;
+        zone_tmp->block_count = per_media_count;
+        zone_tmp->used_count = 0;
+        if(zone_type == zone_block_type)
+        {
+            len = sizeof(storage_media)+(zone->block_count/sizeof(long) +1)*sizeof(long);
+            media = malloc(len);
+            if(media)
+            {
+                memset(media,0,len);
+                media->file_index = 0;
+                media->file.block_file.block_free_count = per_media_count;
+                media->file.root_id = -1;
+                f = fopen(zone_name,"w+");
+                if(f)
+                {
+                    int fd = fileno(f);
+                    posix_fallocate(fd,0,len);
+                    fwrite(zone,sizeof(storage_zone),1,f);
+                    fseek(f,sizeof(storage_zone),SEEK_SET);
+                    fwrite(media,len,1,f);
+                    if(opened_media_file_count < MAX_OPEN_MEDIA_FILE)
+                    {
+                        media->file.block_file.file_handle = f;
+                        open_media_file_count++;
+                    }
+                    else
+                    {
+                        media->file.block_file.file_handle = NULL;
+                        fclose(f);
+                    }
+                }
+            }
+            goto free_zone;
+        }
+        else if(zone_type == zone_free_type)
+        {
+            printf("zone type un supported\r\n");
+            goto free_zone;
+        }
+        else
+        {
+            printf("zone type err\r\n");
+            goto free_zone;
+        }
+        zone_tmp->zone_id = alloc_zone_id();
+        return zone_tmp;
+    }
+    return NULL;
 }
 
+
+int reg_storage_zone(char *zone_name,int zone_type,int block_size,int per_media_count)
+{
+    storage_zone_head *zone_head ;
+    storage_zone *zone_tmp , *zone_pre ;
+    int len = 0;
+    storage_media *media;
+    FILE *f = NULL;
+    if(strlen(zone_name) >=32)
+    {
+        return -1;
+    }
+
+    if(zone_type >=zone_type_max)
+    {
+        return -1;
+    }
+
+    if(NULL == zone_head)
+    {
+        zone_head = malloc(sizeof(storage_zone_head));
+        if(NULL == zone_head) return -1;
+        memset(zone_head,0,sizeof(storage_zone_head);
+    }
+    zone_pre = zone_tmp = zone_head->zone_list;
+    while(zone_tmp)
+    {
+        if(0 = strcmp(zone_tmp->zone_name,zone_name))
+        {
+            return -1;
+        }
+        zone_pre = zone_tmp;
+        zone_tmp = zone_tmp->next;
+    }
+    
+    if(access(zone_name,0)== 0)
+    {
+        zone_tmp = restore_storage_zone(zone_name);
+    } 
+    else
+    {
+        zone_tmp = alloc_storage_zone(zone_name,zone_type,block_size,per_media_count);
+    }
+    
+    if(NULL == zone_tmp)
+    {
+        goto free_zone_head;
+    }
+
+    if(NULL == zone_pre)
+    {
+        zone_head->zone_list = zone_tmp;
+    }
+    else
+    {
+        zone_pre->next = zone_tmp;
+    }
+    zone_head->zone_num++;
+    return zone_tmp->zone_id;
+
+free_zone_head:
+    free(zone_head);
+    return -1;
+}
+
+int reg_storage_buddy_system(char *buddy_name,char order_array[] ,int order_num)
+{
+
+
+
+
+
+    return -1;
+}
+void *alloc_space_from_buddy_sys(int sys_id,int len)
+{
+
+
+}
+storage_zone *get_zone(int zone_id)
+{
+    storage_zone *zone = NULL;
+    if(NULL == zone_head)
+    {
+        return NULL;
+    }
+    zone = zone_head->zone_list;
+    while(zone)
+    {
+        if(zone->zone_id == zone_id)
+        {
+            return zone;
+        }
+    }
+    return NULL;
+}
+
+unsigned long alloc_block_from_zone(int zone_id)
+{
+    storage_zone *zone = get_zone(zone_id);
+    storage_media *media = NULL;
+    unsigned long  block_index = 0;
+    FILE *file;
+    int len = 0;
+    if(zone)
+    {
+        media = zone->cur_media;
+        if(media)
+        {
+            switch(zone->zone_type)
+            {
+                case zone_block_type:
+                {
+                    block_index = find_next_zero_bit(media->file.block_file.bitmap,zone->block_count,0);
+                    len = sizeof(storage_media)+(zone->block_count/sizeof(long) +1)*sizeof(long);
+                    set_bit(block_index,media->file.block_file.bitmap);
+                    media->file.block_file.block_free_count++;
+                    file = media->file_handle;
+                    if(0 == media->file_index)
+                    {
+                        fseek(file,sizeof(storage_zone),SEEK_SET);
+                    }
+                    else
+                    {
+                        fseek(file,0,SEEK_SET);
+                    }
+                    fwrite(media,len,1,file);
+                    return (block_index +media->file_index *zone->block_count)|(zone->zone_id << (BITS_PER_LONG- BITS_PER_BYTE));            
+                }
+                
+                case zone_free_type:
+                default:
+                printf("zone type err\r\n");
+                return -1;
+             }
+        }
+    }
+
+    return -1;
+}
+
+void save_data(unsigned long  id,void* data,int len)
+{
+    unsigned long  index = (id & zone_index_mask)>>(BITS_PER_LONG-BITS_PER_BYTE);    
+    file_head *head = zone_list[index].head;
+    unsigned long offset = zone_list[index].size*((id<<BITS_PER_BYTE)>>(BITS_PER_BYTE))+sizeof(file_head);
+    FILE *file_stream = zone_list[index].file;
+    fseek(file_stream,offset,SEEK_SET);
+    fwrite(data,len,1,file_stream);
+    return ;   
+}
 unsigned long alloc_key_disk(int len)
 {
     unsigned long  i = 0;
---
+++ storage_sys.h
@@ -0,0 +1,74 @@
+#ifndef __storage_sys__
+#define __storage_sys__
+#include <stdio.h>
+
+enum zone_type
+{
+    zone_block_type,
+    zone_free_type,
+    zone_type_max, 
+};
+
+typedef struct storage_block_file_tag
+{
+    int block_free_count;
+    int root_id;
+    long bitmap[0];
+}storage_block_file;
+
+typedef struct storage_free_file_tag
+{
+    int free_size;
+    int free_area_offset;
+    int used_area_offset; 
+}storage_free_file;
+                            
+typedef struct storage_media_tag
+{
+    int file_index;
+    FILE *file_handle;
+    struct storage_media_tag *next;
+    union
+    {
+        storage_block_file block_file;
+        storage_free_file  free_file;
+    }file;
+}storage_media;
+
+typedef struct storage_zone_tag
+{
+    char zone_name[32];
+    int  zone_id;
+    int  zone_type;
+    int  block_size;
+    int  block_count;
+    int  media_count;
+    storage_media *media_list;
+    storage_media *cur_media;
+    struct storage_zone *next;    
+}storage_zone;
+
+typedef struct storage_zone_head_tag
+{
+    int zone_num;
+    storage_zone *zone_list;
+}storage_zone_head;
+
+typedef struct storage_buddy_sys_tag
+{
+    char buddy_sys_name[32];
+    int order_num;
+    int order_min;
+    int order_max;
+    int order_array[8];
+    storage_zone block_zone[8];
+    storage_zone free_zone; 
+}storage_buddy_sys;
+
+typedef struct storage_buddy_head_tag
+{
+    int sys_num;
+    storage_buddy_sys *sys_list;
+}storage_buddy_head;
+
+#endif
