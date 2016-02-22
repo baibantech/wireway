359c1742f69ae3d5d3a868842bee422a8116a718
lijiyong0303 <lijiyong0303@outlook.com>
2016-02-21 23:54:22 -0800

    begin user entity function

---
+++ user_entity.h
@@ -0,0 +1,27 @@
+#ifndef __user_entity__
+#define __user_entity__
+
+
+
+typedef relate_point_desc
+{
+    
+
+
+}relate_point;
+
+typedef user_entity_desc_tag
+{
+    char *name;
+    unsigned long user_id;
+       
+}user_entity_desc;
+
+
+
+
+
+
+
+
+#endif
