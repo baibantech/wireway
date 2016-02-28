3f3faed02171d3e94bc28505625487fec1bdef75
lijiyong0303 <lijiyong0303@outlook.com>
2016-02-28 00:45:47 -0800

    begin user entity function

--- user_entity.c
+++ user_entity.c
@@ -1,2 +1,37 @@
 #include "user_entity.h"
+#include "bptree.h"
+typedef struct reg_entity_req
+{
+    char *name;
+    unsigned long reg_token;
+    
+}reg_entity_req;
+
+
+tree_root user_entity_control
+{
+
+
+
+}
+
+
+
+unsigned long prepare_reg_entity()
+{
+
+
+
+
+}
+
+
+user_entity_desc *reg_entity()
+{
+
+
+
+
+}
+
 
--- user_entity.h
+++ user_entity.h
@@ -1,27 +1,58 @@
 #ifndef __user_entity__
 #define __user_entity__
-
-
-
-typedef relate_point_desc
+#include "wireway.h"
+typedef struct relate_point_desc
 {
+    int index;
+    int port_index;
+    point *p;
+}relate_wireway_point;
+
+typedef struct relate_point_array
+{
+    int num;
+    relate_wireway_point[100];
+    relate_point_array *next_array;
+}relate_point_array;
+typedef struct user_port_addr
+{
+    union {
+        unsigned long addr;
+        char addr_string[64];
+    }port_logic_addr;
     
+    union {
+        unsigned long phy_addr;
+        char phy_addr_string[64];
+    }port_phy_addr;    
 
+    struct user_port_addr *next;
+}user_port_addr;
 
-}relate_point;
+typedef struct user_logic_port_tag
+{
+    char port_name[32];
+    int port_index;
+    int port_type;
+    int port_state;
+    user_port_addr *port_addr;
+}user_logic_port;
 
-typedef user_entity_desc_tag
+typedef struct user_entity_desc_tag
 {
     char *name;
     unsigned long user_id;
-       
+    unsigned long user_block_id;
+    user_logic_port *port;
+    relate_point_array *owner_point;
+    relate_point_array *attach_point;   
 }user_entity_desc;
 
 
-
-
-
-
-
+//int lookup_resource_server();
+//int reg_to_resource_server(); 
+int prepare_reg_entity();
+int reg_entity();
+int addr_entity_port();
 
 #endif
