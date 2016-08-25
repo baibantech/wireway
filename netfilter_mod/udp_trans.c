c3b53a8e85b143060fdfd2190a724fffc13bf720
lijiyong0303 <lijiyong0303@outlook.com>
2016-08-25 01:59:39 -0700

    add udp trans function

---
+++ netfilter_mod/udp_trans.c
@@ -0,0 +1,31 @@
+#include <linux/kernel.h>
+
+extern struct net init_net; //sock kern net namespace
+
+
+int send_udp_packet_no_fagment(int dst_ip,int dst_port ,void *data,int size)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
