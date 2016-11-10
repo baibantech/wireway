97c7e186475ffc22f587171fbd3f25c6c8b0954d
lijiyong0303 <lijiyong0303@outlook.com>
2016-11-09 23:27:23 -0800

    process use mem_merge dev

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
     
 #include <stdio.h>  
 #include <stdlib.h>  
-  
+#include <error.h>  
 #include <string.h>  
 #include <sys/socket.h>  
 #include <netinet/in.h>  
 #include <arpa/inet.h>  
 #include <netdb.h>  
-
+#include <fcntl.h>
 #include "user_entity.h"
- 
+#include <sys/ioctl.h> 
+#define MERGE_IOCTL_MAGIC 0xF6
+
+
+#define MERGE_IOC_SHOW_VMA _IO(MERGE_IOCTL_MAGIC,1) 
+
 int port=6789; 
 
 int init_deamon()
@@ -31,12 +36,23 @@ int init_deamon()
   
 int main(int argc,char** argv)
 {
-    
+    int fd  = 0;
     init_deamon();
-    printf("init socket \r\n");
+   
+    #if 0
+     printf("init socket \r\n");
     wireway_thread_init();
     sleep(10);
     entity_test_thread_init();
+    #endif
+    fd = open("/dev/merge_mem",O_RDWR);
+    if(fd == -1)
+    {
+        perror("open");
+        printf("err open dev\r\n");
+    }     
+    ioctl(fd,MERGE_IOC_SHOW_VMA ,NULL);
+    close(fd);
     while(1)
     {
         sleep(1);
